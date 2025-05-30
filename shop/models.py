f208f55b4edcf48f9e6946a8b600c4016eb590cd
Natha <nathansloan2003@gmail.com>
2025-05-30 13:42:58 -0400

    default files

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
 # Create your models here.
+
+# class - defines an object
+# models.Model - states object is a Django Model, saves to database
+#class Quote(models.Model):
